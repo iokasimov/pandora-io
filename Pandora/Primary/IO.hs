c784cc4a4966a80e1c1bd9b54ea5d7918cc20e06
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-30 15:12:18 +0300

    Reexport IO and implement few instances for it

---
+++ Pandora/Primary/IO.hs
@@ -0,0 +1,34 @@
+module Pandora.Primary.IO (IO) where
+
+import "pandora" Pandora.Core.Morphism ((.))
+import "pandora" Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import "pandora" Pandora.Pattern.Functor.Pointable (Pointable (point))
+import "pandora" Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
+import "pandora" Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
+
+import "ghc-prim" GHC.Prim (State#, RealWorld)
+import "ghc-prim" GHC.Types (IO (IO))
+
+instance Covariant IO where
+	f <$> x = bindIO x (returnIO . f)
+
+instance Pointable IO where
+	point = returnIO
+
+instance Applicative IO where
+	x <*> y = x >>= \x' -> y >>= \y' -> returnIO (x' y')
+
+instance Bindable IO where
+	(>>=) = bindIO
+
+returnIO :: a -> IO a
+returnIO x = IO (\ s -> (# s, x #))
+
+bindIO :: IO a -> (a -> IO b) -> IO b
+bindIO (IO m) k = IO (\s -> case m s of (# new_s, a #) -> unIO (k a) new_s)
+
+thenIO :: IO a -> IO b -> IO b
+thenIO (IO m) k = IO (\s -> case m s of (# new_s, _ #) -> unIO k new_s)
+
+unIO :: IO a -> (State# RealWorld -> (# State# RealWorld, a #))
+unIO (IO a) = a
