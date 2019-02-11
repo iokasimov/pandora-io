bd856fef6718b0d339010e334a79cb4aeb763fa9
Murat Kasimov <iokasimov.m@gmail.com>
2019-02-11 23:22:27 +0300

    Import some literal operatations on

--- Pandora/Primary/Literals.hs
+++ Pandora/Primary/Literals.hs
@@ -1,3 +1,4 @@
-module Pandora.Primary.Literals (Char, Int, Word, Float, Double) where
+module Pandora.Primary.Literals (Char, Int, Word, Float, Double, ord#, (+#), (-#), (*#)) where
 
+import "ghc-prim" GHC.Prim (ord#, (+#), (-#), (*#))
 import "ghc-prim" GHC.Types (Char, Int, Word, Float, Double)
