6747de926e9c3cf8bbf398e1d6e49f2c07080f49
Murat Kasimov <iokasimov.m@gmail.com>
2020-04-11 13:43:49 +0300

    Update Pandora version

--- Pandora/IO.hs
+++ Pandora/IO.hs
@@ -2,8 +2,7 @@ module Pandora.IO (IO, module Exports) where
 
 import Pandora.IO.Bytes as Exports
 
-import "pandora" Pandora.Core.Morphism ((.))
-import "pandora" Pandora.Paradigm.Junction.Transformer (T (T, t), type (:!:))
+import "pandora" Pandora.Pattern.Category ((.))
 import "pandora" Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import "pandora" Pandora.Pattern.Functor.Pointable (Pointable (point))
 import "pandora" Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
