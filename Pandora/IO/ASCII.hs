04139cc4f1d893162501caa0298c61c595283771
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-14 14:12:27 +0300

    Remove Variation data family

--- Pandora/IO/ASCII.hs
+++ Pandora/IO/ASCII.hs
@@ -3,16 +3,20 @@ module Pandora.IO.ASCII where
 data Letter = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
 
 data family Case a
-data instance Case Letter = Uppercase | Lowercase
+data instance Case Letter = Upper | Lower
 
 data Number = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
+
+data Sign = Exclamation | Quote Quote | Hash | Dollar
+	| Percent | Ampersand | Apostrophe | Asterisk
+	| Plus | Comma | Minus | Period | Slash Slash
+	| Colon | Semicolon | At | Caret | Underscore | Accent | Bar | Tilde
 
 data Bracket = Round | Square | Angle | Curly
 
 data family Position a
 data instance Position Bracket = Opened | Closed
 
-data Sign = Exclamation | Doublequote | Hash | Dollar
-	| Percent | Ampersand | Apostrophe | Asterisk
-	| Plus | Comma | Minus | Period | Slash | At
-	| Backslash | Caret | Underscores | Accent | Bar | Tilde
+data Slash = Forward | Back
+
+data Quote = Double | Single
