ceecb606f40bbd0c5fb823e0cdd65253e3d3b731
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-14 13:55:37 +0300

    Define digits and signs in ASCII module

--- Pandora/IO/ASCII.hs
+++ Pandora/IO/ASCII.hs
@@ -5,7 +5,14 @@ data Letter = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q 
 data family Case a
 data instance Case Letter = Uppercase | Lowercase
 
+data Number = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
+
 data Bracket = Round | Square | Angle | Curly
 
 data family Position a
 data instance Position Bracket = Opened | Closed
+
+data Sign = Exclamation | Doublequote | Hash | Dollar
+	| Percent | Ampersand | Apostrophe | Asterisk
+	| Plus | Comma | Minus | Period | Slash | At
+	| Backslash | Caret | Underscores | Accent | Bar | Tilde
