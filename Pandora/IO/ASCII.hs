627d30c369e53652a2b2bd50bc31e977f734569c
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-14 14:21:51 +0300

    Define ASCII character datatype

--- Pandora/IO/ASCII.hs
+++ Pandora/IO/ASCII.hs
@@ -1,22 +1,27 @@
 module Pandora.IO.ASCII where
 
-data Letter = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
+data ASCII = Control Control | Sign Sign | Letter Case Letter | Number Number
 
-data family Case a
-data instance Case Letter = Upper | Lower
+data Control = NUL | SOH | STX | ETX | EOT | ENQ
+	| ACK | BEL | BS | HT | LF | VT | FF | CR | SO | SI
+	| DLE | DC1 | DC2 | DC3 | DC4 | NAK | SYN | ETB | CAN
+	| EM | SUB | ESC | FS | GS | RS | US | SP | DEL
 
-data Number = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
-
-data Sign = Exclamation | Quote Quote | Hash | Dollar
-	| Percent | Ampersand | Apostrophe | Asterisk
-	| Plus | Comma | Minus | Period | Slash Slash
+data Sign = Exclamation | Quote Quote | Hash | Dollar | Percent | Ampersand | Apostrophe
+	| Asterisk | Plus | Comma | Minus | Period | Slash Slash | Bracket Position Bracket
 	| Colon | Semicolon | At | Caret | Underscore | Accent | Bar | Tilde
 
 data Bracket = Round | Square | Angle | Curly
 
-data family Position a
-data instance Position Bracket = Opened | Closed
+data Position = Opened | Closed
 
 data Slash = Forward | Back
 
 data Quote = Double | Single
+
+data Letter = A | B | C | D | E | F | G | H | I | J | K | L
+	| M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
+
+data Case = Upper | Lower
+
+data Number = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
