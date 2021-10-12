d2c2085e52414fe20afc413757c5058ea00470f2
Murat Kasimov <iokasimov.m@gmail.com>
2021-10-12 09:59:03 +0300

    Update Pandora version

--- Pandora/IO.hs
+++ Pandora/IO.hs
@@ -3,14 +3,15 @@ module Pandora.IO (IO, module Exports) where
 import Pandora.IO.ASCII as Exports
 import Pandora.IO.Bytes as Exports
 
+import "pandora" Pandora.Pattern.Morphism.Straight (Straight (Straight))
 import "pandora" Pandora.Pattern.Semigroupoid ((.))
 import "pandora" Pandora.Pattern.Category (($))
-import "pandora" Pandora.Pattern.Functor.Covariant (Covariant ((-<$>-)))
-import "pandora" Pandora.Pattern.Functor.Semimonoidal (Semimonoidal (multiply))
-import Pandora.Pattern.Functor.Monoidal (Monoidal (unit))
+import "pandora" Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import "pandora" Pandora.Pattern.Functor.Semimonoidal (Semimonoidal (mult))
+import "pandora" Pandora.Pattern.Functor.Monoidal (Monoidal (unit))
 import "pandora" Pandora.Pattern.Functor.Bindable (Bindable ((=<<)))
 import "pandora" Pandora.Pattern.Functor.Monad (Monad)
-import "pandora" Pandora.Paradigm.Primary.Algebraic.Exponential ()
+import "pandora" Pandora.Paradigm.Primary.Algebraic.Exponential (type (-->))
 import "pandora" Pandora.Paradigm.Primary.Algebraic.Product ((:*:)((:*:)))
 import "pandora" Pandora.Paradigm.Primary.Algebraic.One (One (One))
 import "pandora" Pandora.Paradigm.Primary.Algebraic ()
@@ -19,18 +20,18 @@ import "ghc-prim" GHC.Prim (State#, RealWorld)
 import "ghc-prim" GHC.Types (IO (IO))
 
 instance Covariant (->) (->) IO where
-	f -<$>- x = bindIO x (returnIO . f)
+	f <$> x = bindIO x (returnIO . f)
 
-instance Semimonoidal (->) (:*:) (:*:) IO where
-	multiply (x :*: y) = bindIO x $ \x' -> bindIO y $ \y' -> returnIO (x' :*: y')
+instance Semimonoidal (-->) (:*:) (:*:) IO where
+	mult = Straight $ \(x :*: y) -> bindIO x $ \x' -> bindIO y $ \y' -> returnIO (x' :*: y')
 
-instance Monoidal (->) (->) (:*:) (:*:) IO where
-	unit _ f = returnIO $ f One
+instance Monoidal (-->) (->) (:*:) (:*:) IO where
+	unit _ = Straight $ returnIO . ($ One)
 
 instance Bindable (->) IO where
 	f =<< x = bindIO x f
 
-instance Monad IO where
+instance Monad (->) IO where
 
 returnIO :: a -> IO a
 returnIO x = IO (\ s -> (# s, x #))
